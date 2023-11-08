9d61f8ecf4c6463a73fd718fe3ce5da8ce82ca79
HoseinRezaei <rezaeen77@gmail.com>
2023-11-08 18:04:17 +0330

    @DiscriminatorValue("Car")

--- src/main/java/org/hibernate/inheritance/entity/FourWheeler.java
+++ src/main/java/org/hibernate/inheritance/entity/FourWheeler.java
@@ -4,9 +4,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("Car")
 @Setter
 @Getter
 @NoArgsConstructor
