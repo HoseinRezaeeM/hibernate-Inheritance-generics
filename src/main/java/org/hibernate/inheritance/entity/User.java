eec6641e7d4757d39ca29b46102936bd92c53ee4
HoseinRezaei <rezaeen77@gmail.com>
2023-11-12 15:24:49 +0330

    NamedQuery and NamedNativeQuery

--- src/main/java/org/hibernate/inheritance/entity/User.java
+++ src/main/java/org/hibernate/inheritance/entity/User.java
@@ -12,11 +12,13 @@ import java.util.List;
 @AllArgsConstructor
 @Table(name = "users")
 @ToString
+@NamedQuery(name = "user.byid",query = "SELECT u FROM User u WHERE id > :id")
+@NamedNativeQuery(name = "user.byname",query = "SELECT * FROM postgres.public.users WHERE name=:name",resultClass = User.class)
 public class User {
     @Id @GeneratedValue
     private Integer id;
     private String name;
-    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
     @JoinTable(name = "users_vehicle" ,joinColumns = @JoinColumn(name = "user_id")
             ,inverseJoinColumns = @JoinColumn(name = "vehicle_id"))
     private List<Vehicle> list;
