d9c8dd59389b2e2bc64e9c01e5090f93c223a7e7
HoseinRezaei <rezaeen77@gmail.com>
2023-11-12 16:03:03 +0330

    Write Criteria

--- src/main/java/org/hibernate/inheritance/Application.java
+++ src/main/java/org/hibernate/inheritance/Application.java
@@ -1,6 +1,8 @@
 package org.hibernate.inheritance;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.inheritance.config.SessionFactoryProvider;
 import org.hibernate.inheritance.entity.FourWheeler;
 import org.hibernate.inheritance.entity.TwoWheeler;
@@ -17,30 +19,32 @@ public class Application {
     public static void main(String[] args) {
 
 
-
-        TwoWheeler vehicleTwo =new TwoWheeler("bmw");
-        FourWheeler vehicleFour =new FourWheeler("benz");
-        List<Vehicle> list = Arrays.asList(vehicleTwo,vehicleFour);
-        User user =new User(null,"Hosein",list);
-
+        TwoWheeler vehicleTwo = new TwoWheeler("bmw");
+        FourWheeler vehicleFour = new FourWheeler("benz");
+        List<Vehicle> list = Arrays.asList(vehicleTwo, vehicleFour);
+        User user = new User(null, "Hosein", list);
 
 
         Session session = SessionFactoryProvider.getSessionFactory().openSession();
 //        try {
-           session.beginTransaction();
+        session.beginTransaction();
 //        //    final Vehicle vehicle1 = session.get(Vehicle.class, 2);
 //          //  System.out.println(vehicle1);
 ////            session.save(vehicle);
 ////            session.save(vehicleTwo);
 ////            session.save(vehicleFour);
-           session.save(user);
+        session.save(user);
 
 //           // String id ="1";
 ////            final TypedQuery query = session.createQuery("UPDATE FROM Vehicle SET VehicleName=:v where Id=:i");
 ////            query.setParameter("v","xanzi");
 ////           query.setParameter("i",43);
 ////           query.executeUpdate();
-//
+        Criteria criteria = session.createCriteria(User.class);
+        criteria.add(Restrictions.eq("name", "Hosein"));
+        final List list3 = criteria.list();
+        System.out.println(list3);
+
 //            // query.setFirstResult(1);   //first data
 //          //  query.setMaxResults(2);   // max data
 //        //   final List<String> list = query.list();
@@ -48,7 +52,7 @@ public class Application {
 ////                System.out.println(v);
 ////            }
 //       //    System.out.println(list);
-            session.getTransaction().commit();
+        session.getTransaction().commit();
 //        }catch (Exception e){
 //            e.printStackTrace();
 //            session.getTransaction().rollback();
@@ -57,15 +61,17 @@ public class Application {
 //        final List<User> list1 = select_u_from_user_u.list();
 //        System.out.println(list1);
         final Query namedQuery = session.getNamedQuery("user.byid");
-        namedQuery.setParameter("id",1);
+        namedQuery.setParameter("id", 1);
         final List list1 = namedQuery.list();
         System.out.println(list1);
 
 
         final Query namedQuery1 = session.getNamedNativeQuery("user.byname");
-        namedQuery1.setParameter("name","Hosein");
+        namedQuery1.setParameter("name", "Hosein");
         final List list2 = namedQuery1.list();
         System.out.println(list2);
+
+
     }
 
 
